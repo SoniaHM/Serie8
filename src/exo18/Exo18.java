6dc4e5640f5184c50671d7b43ee73216b2bf1d2d
asano <asano@192.168.0.22>
2021-03-22 00:21:27 +0100

    modified list of person in Exo18

--- src/exo18/Exo18.java
+++ src/exo18/Exo18.java
@@ -3,8 +3,8 @@ package exo18;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
+
 
 public class Exo18 {
 	public static void main(String[] args) {
@@ -15,22 +15,16 @@ public class Exo18 {
 		System.out.println("\n Question 2: \n");
 		System.out.println("Nous devons ajouter une implementation de l'interface Serializable du java.io\n");
 		
-		List<Person> perso= new ArrayList<>();
-		Person p1 = new Person ("Zineddine","Ziden",25);
-		Person p2 = new Person ("Ronaldo","Jr",30);
-		Person p3 = new Person ("Beckham","David",35);
-		Person p4 = new Person ("Figo","Luis",40);
-		Person p5 = new Person ("Casillas","Iker",45);
-		Person tab[]= {p1,p2,p3,p4,p5};
-		for(int i=0;i<tab.length;i++) {
-			perso.add(tab[i]);
-		}
+		List<Person> newPerson= List.of(
+				new Person ("Kylian","Mbappe",22),
+				new Person ("José","Paummard",40));
+		
         String fileName = "files/newfile2.bin";
 
         System.out.println("\n Question 3: \n");
         System.out.println("Check the file 'newfile2.bin' \n");
         try (PersonOutputStream personOutputStream = new PersonOutputStream(new FileOutputStream(fileName))) {
-            personOutputStream.writePeople(perso);
+            personOutputStream.writePeople(newPerson);
         } catch (IOException e) {
             e.printStackTrace();
         }
